926ca06f3c11f8430a3cb6fa2aafc9faf0c1e59b
IMBABOT1 <zlovred12@gmail.com>
2021-11-20 14:33:14 +0300

    movement()

--- core/src/com/mygdx/game/Snake.java
+++ core/src/com/mygdx/game/Snake.java
@@ -18,7 +18,10 @@ public class Snake {
     }
 
     private Vector2 position;
-    private float velocity;
+    private float velocityRight;
+    private float velocityLeft;
+    private float velocityUp;
+    private float velocityDown;
     private int offset;
 
     private int left;
@@ -29,12 +32,18 @@ public class Snake {
     private int score;
     private StringBuilder builder;
 
+    private int speed;
+
 
 
     public Snake(TextureAtlas atlas){
         this.position = new Vector2(100, 100);
         this.texture = atlas.findRegion("Snake");
-        this.velocity = 10;
+        this.speed = 5;
+        this.velocityRight = 0;
+        this.velocityLeft = 0;
+        this.velocityUp = 0;
+        this.velocityDown = 0;
         this.offset = 20;
         this.left = 0;
         this.bottom = 0;
@@ -42,6 +51,7 @@ public class Snake {
         this.top = 600;
         this.score = 0;
         this.builder = new StringBuilder();
+
     }
 
     public void render(SpriteBatch batch){
@@ -59,28 +69,54 @@ public class Snake {
     }
 
     public void update(float dt){
-        checkCollisions();
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            position.x -= velocity;
-        }else if (Gdx.input.isKeyPressed(Input.Keys.D)){
-            position.x += velocity;
-        }else if (Gdx.input.isKeyPressed(Input.Keys.S)){
-            position.y -= velocity;
-        }else if (Gdx.input.isKeyPressed(Input.Keys.W)){
-            position.y += velocity;
-        }
+        checkBounds();
+        movement();
+
     }
 
 
-    private void checkCollisions(){
+    private void movement(){
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            velocityRight = speed;
+            velocityLeft = 0;
+            velocityUp = 0;
+            velocityDown = 0;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            velocityRight = 0;
+            velocityLeft = speed;
+            velocityUp = 0;
+            velocityDown = 0;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            velocityDown = 0;
+            velocityUp = speed;
+            velocityLeft = 0;
+            velocityRight = 0;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            velocityDown = speed;
+            velocityUp = 0;
+            velocityRight = 0;
+            velocityLeft = 0;
+        }
+        position.x += velocityRight;
+        position.x -= velocityLeft;
+        position.y += velocityUp;
+        position.y -= velocityDown;
+    }
+
+
+
+    private void checkBounds(){
         if (position.x > right){
             position.x = right;
         }
         if (position.x < left + offset){
             position.x = left + offset;
         }
-        if (position.y > top){
-            position.y = top;
+        if (position.y > top - offset){
+            position.y = top - offset;
         }
         if (position.y < bottom + offset){
             position.y = bottom + offset;
